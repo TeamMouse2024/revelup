dabbc151faccb060bc0249cbce95e4c35f3eeebb
KIMHYEONJI13 <https://github.com/KIMHYEONJI13>
2024-03-11 09:34:54 +0900

    mypage

--- src/main/java/com/revelup/funding/model/dto/DeliveryDTO.java
+++ src/main/java/com/revelup/funding/model/dto/DeliveryDTO.java
@@ -1,4 +1,18 @@
 package com.revelup.funding.model.dto;
 
-public class DeliveryDTO {
+import com.revelup.pay.model.dto.PayDTO;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class DeliveryDTO implements java.io.Serializable {
+
+    private PayDTO plgCode;
+    private String delivStat;
+    private String trackingNo;
+
+
 }
--- src/main/java/com/revelup/funding/model/dto/GiftDTO.java
+++ src/main/java/com/revelup/funding/model/dto/GiftDTO.java
@@ -1,4 +1,18 @@
 package com.revelup.funding.model.dto;
 
-public class GiftDTO {
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class GiftDTO implements java.io.Serializable {
+
+    private FundingInfoDTO fndCode;
+    private int giftPrice;
+    private String giftName;
+    private int giftProdQty;
+
+
 }
